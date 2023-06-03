2299f3ccede547ef27bbf54bd0e57cb50129830a
petersolimine <peter@beulr.com>
2023-06-03 12:28:43 -0700

    remove env

--- myenv/lib/python3.11/site-packages/numba/cuda/simulator/compiler.py
+++
@@ -1,7 +0,0 @@
-'''
-The compiler is not implemented in the simulator. This module provides a stub
-to allow tests to import successfully.
-'''
-
-compile_ptx = None
-compile_ptx_for_current_device = None
